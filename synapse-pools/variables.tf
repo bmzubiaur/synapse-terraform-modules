0c396bdada7795c9e4cfe8ac2c5d13b8f3105c7e
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-17 15:22:34 -0500

    Update variables.tf

--- synapse-pools/variables.tf
+++ synapse-pools/variables.tf
@@ -22,5 +22,4 @@ variable "enable_syn_sparkpool" {
 variable "spark_version" {
   description = "Variable to set the Apache Spark version for a synapse spark cluster"
   type        = string
-  default     = "3.2"
 }
