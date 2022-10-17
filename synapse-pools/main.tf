da276b8b366cb651fe6e7c45525173fb173c0a0d
bmzubiaur <114438280+bmzubiaur@users.noreply.github.com>
2022-10-17 15:48:33 -0500

    Update main.tf

--- synapse-pools/main.tf
+++ synapse-pools/main.tf
@@ -9,22 +9,3 @@ resource "azurerm_synapse_sql_pool" "syn_syndp" {
   count                = var.enable_syn_sqlpool ? 1 : 0
 }
 
-# Spark Pool 
-resource "azurerm_synapse_spark_pool" "syn_synsp" {
-  name                 = "synsp${var.postfix}"
-  synapse_workspace_id = var.synapse_workspace_id
-  node_size_family     = "MemoryOptimized"
-  node_size            = "Small"
-  count                = var.enable_syn_sparkpool ? 1 : 0
-  spark_log_folder     = "/logs2"
-  spark_version        = "3.2"
-
-  auto_scale {
-    max_node_count = 50
-    min_node_count = 3
-  }
-
-  auto_pause {
-    delay_in_minutes = 15
-  }
-}
